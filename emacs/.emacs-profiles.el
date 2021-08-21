7ae7769a38c4875e17ed452cf04d1dbdfc2b42f7
Chris Hunt <chrahunt@gmail.com>
2021-08-21 11:43:24 -0400

    emacs: switch to Doom by default

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,3 +1,3 @@
-(("default" . ((user-emacs-directory . "~/.emacs.d-spacemacs")))
- ("doom" . ((user-emacs-directory . "~/.emacs.d-doom")
+(("spacemacs" . ((user-emacs-directory . "~/.emacs.d-spacemacs")))
+ ("default" . ((user-emacs-directory . "~/.emacs.d-doom")
             (env . (("DOOMLOCALDIR" . "~/.emacs.d-doom-.local/"))))))
