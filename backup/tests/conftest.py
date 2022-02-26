edb6e07df2b1e82760d08daac9aa296a92873421
Chris Hunt <chrahunt@gmail.com>
2022-02-26 11:07:16 -0500

    backup: Update docs and type hints
    No behavior change.

--- backup/tests/conftest.py
+++ backup/tests/conftest.py
@@ -3,6 +3,17 @@ import pytest
 
 @pytest.fixture
 def ok():
+    """Accumulate expected outputs defined alongside other inputs.
+
+    Example:
+        inputs = [
+            0 > ok,
+            1,
+            2 > ok,
+        ]
+        expected = list(ok)
+        assert even_numbers(inputs) == expected
+    """
     class Ok:
         def __init__(self):
             self.value = []
